a6599c3d187557c9bbee77c8ad297e2171566412
Sneha <sg927100000@gmail.com>
2026-03-30 20:53:43 +0530

    Setup Express server with basic route

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+export default app;

7b79c6a87f4a839019fd1b48d8c499e358e53131
Sneha <sg927100000@gmail.com>
2026-04-13 23:16:01 +0530

    feat: add admin role-based access middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -27,3 +27,15 @@ try {
   });
 }
 };
+
+export const adminMiddleware = (req, res, next) => {
+    if (req.user.role !== "admin") {
+  return res.status(403).json({
+    success: false,
+    message: "Access denied: Admins only"
+  });
+}
+
+next();
+
+};
